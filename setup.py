62a06fc5a974b49e577f8385873c14b53bc5c5e0
syedimranhassan <imran.hassan@arbisoft.com>
2020-02-20 13:33:05 +0500

    test files

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='test-imran',
+    version='0.0.1',
+    description='Celery tasks supporting the operations of edX\'s ecommerce service',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+    ],
+    keywords='ihassan test jenkins',
+    url='https://github.com/syedimranhassan/ihpkg',
+    author='ihassan',
+    author_email='ihassan@edx.org',
+    license='AGPL',
+    packages=find_packages(exclude=['*.tests']),
+    install_requires=[
+        'celery>=3.1.18,<4.0.0',
+        'edx-rest-api-client>=1.5.0,<2.0.0'
+    ],
+)
+© 2020 GitHub, Inc.
