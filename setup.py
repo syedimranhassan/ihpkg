0744c3f141013cd25a8c1a6c738c7f081d56df02
syedimranhassan <45480841+syedimranhassan@users.noreply.github.com>
2020-02-26 15:08:28 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
     long_description = readme.read()
 
 setup(
-    name='test-nomore',
+    name='test-bachat',
     version='0.0.1',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
