aefbb8d5c4423f729b167535cb59f97d926765f4
syedimranhassan <45480841+syedimranhassan@users.noreply.github.com>
2020-03-18 18:14:06 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme:
 
 setup(
     name='test-zongissue',
-    version='0.0.7',
+    version='0.0.8',
     description='Celery tasks supporting the operations of edX\'s ecommerce service',
     long_description=long_description,
     classifiers=[
